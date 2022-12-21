eb746813d8fe8507616bb816ec4b78454b290205
KwonSeRim2 <117977227+KwonSeRim2@users.noreply.github.com>
2022-12-21 23:09:52 +0900

    Update sketch.js
    style

--- www/sketch.js
+++ www/sketch.js
@@ -25,8 +25,8 @@ function setup() {
   //createCanvas(displaywidth, displayheight)
 
  
-  slider = createSlider(0,10,5);
-  slider.position(100, 150);
+  slider = createSlider(0,10,10);
+  slider.position(250, 400);
   
   //wave.start();
  
@@ -36,8 +36,8 @@ function setup() {
 
    
    button[i] = createButton(String.fromCharCode(65+i%7));
-   button[i].position(100+40*i,100);
-   button[i].size(40,40);
+   button[i].position(120+80*(i%5),200+80*int(i/5));
+   button[i].size(70,70);
    button[i].style('font-size', '30px');
    
   }
@@ -56,6 +56,7 @@ function setup() {
   r = random(50, 255);
   g = random(0,200);
   b = random(50,255);
+  
 }
 
  function draw() {
@@ -71,21 +72,25 @@ function toggle0(){
   if(!playing0){
     wave[0].start();
     wave[0].freq(220);
+    button[0].style("color", "#00b5e2")
     playing0 = true;
   } 
   else {
     wave[0].stop();
+    button[0].style("color", "#000000")
     playing0 = false
   }
 }function toggle1(){
   if(!playing1){
     wave[1].start();
     wave[1].freq(246.9417);
+    button[1].style("color", "#00b5e2")
     playing1 = true;
   } 
   else {
     
     wave[1].stop();
+    button[1].style("color", "#000000")
     playing1= false
   }
 }
@@ -93,10 +98,12 @@ function toggle2(){
   if(!playing2){
     wave[2].start();
     wave[2].freq(261.6256);
+    button[2].style("color", "#00b5e2")
     playing2 = true;
   } 
   else {
     wave[2].stop();
+    button[2].style("color", "#000000")
     playing2 = false
   }
 }
@@ -104,10 +111,12 @@ function toggle3(){
   if(!playing3){
     wave[3].start();
     wave[3].freq(293.6648);
+    button[3].style("color", "#00b5e2")
     playing3 = true;
   } 
   else {
     wave[3].stop();
+    button[3].style("color", "#000000")
     playing3 = false
   }
 }
@@ -115,10 +124,12 @@ function toggle4(){
   if(!playing4){
     wave[4].start();
     wave[4].freq(329.6276);
+    button[4].style("color", "#00b5e2")
     playing4 = true;
   } 
   else {
     wave[4].stop();
+    button[4].style("color", "#000000")
     playing4 = false
   }
 }
@@ -126,10 +137,12 @@ function toggle5(){
   if(!playing5){
     wave[5].start();
     wave[5].freq(349.2282);
+    button[5].style("color", "#00b5e2")
     playing5 = true;
   } 
   else {
     wave[5].stop();
+    button[5].style("color", "#000000")
     playing5 = false
   }
 }
@@ -137,10 +150,12 @@ function toggle6(){
   if(!playing6){
     wave[6].start();
     wave[6].freq(391.9954);
+    button[6].style("color", "#00b5e2")
     playing6 = true;
   } 
   else {
     wave[6].stop();
+    button[6].style("color", "#000000")
     playing6 = false
   }
 }
@@ -148,10 +163,12 @@ function toggle7(){
   if(!playing7){
     wave[7].start();
     wave[7].freq(440);
+    button[7].style("color", "#00b5e2")
     playing7 = true;
   } 
   else {
     wave[7].stop();
+    button[7].style("color", "#000000")
     playing7 = false
   }
 }
@@ -159,10 +176,12 @@ function toggle8(){
   if(!playing8){
     wave[8].start();
     wave[8].freq(493.8833);
+    button[8].style("color", "#00b5e2")
     playing8 = true;
   } 
   else {
     wave[8].stop();
+    button[8].style("color", "#000000")
     playing8 = false
   }
 }
@@ -170,10 +189,12 @@ function toggle9(){
   if(!playing9){
     wave[9].start();
     wave[9].freq(523.2511);
+    button[9].style("color", "#00b5e2")
     playing9 = true;
   } 
   else {
     wave[9].stop();
+    button[9].style("color", "#000000")
     playing9 = false
   }
 }
